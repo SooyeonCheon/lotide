368360baab3f0b3ae6a2a5e2eb4885b363a34083
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-06 17:25:21 -0800

    tail function

---
+++ tail.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); 
+assertEqual(words.length, 3); 
+console.log(tail([1,2,3]));
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
