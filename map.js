76608bcbfa40f07ef9ef82aa27c4cd398b4a0b3a
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-09 15:09:46 -0800

    map file added

---
+++ map.js
@@ -0,0 +1,32 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log('Those two arrays are equal');
+  } else {
+    console.log('Those two arrays are not equal');
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const result1 = map(words, word => word[0]);
+
+
+console.log(result1);
+assertArraysEqual(result1, ['g', 'c', 't', 'm', 't']);
