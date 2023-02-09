8b7a7ccdfd1657b2f38d248e370b064d02327c30
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-09 15:32:25 -0800

    take until lint modified

--- takeUntil.js
+++ takeUntil.js
@@ -1,13 +1,29 @@
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log('Those two arrays are equal');
+  } else {
+    console.log('Those two arrays are not equal');
+  }
+};
 const takeUntil = function(array, callback) {
   let result = [];
-  for(let item of array){
-    if(callback(item)){
+  for (let item of array) {
+    if (callback(item)) {
       return result;
     }
     result.push(item);
   }
-
-}
+};
 
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
@@ -23,3 +39,6 @@ console.log(results2);
 // [ 1, 2, 5, 7, 2 ]
 // --
 // [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
