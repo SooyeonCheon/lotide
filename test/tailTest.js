76ed325f22e76dfcd599cd7ecd4611e9822bba9d
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-13 13:23:55 -0800

    middle&tail test with mocha-chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,16 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3); 
-console.log(tail([1,2,3]));
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+
+  it(`returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse","Labs"]);
+  })
+
+  it(`returns [2, 3, 4] for [1, 2, 3, 4]`, () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  })
+
+})
