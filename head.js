986954ac651f1621c9c8b61ca9a775d56c74e64f
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-13 09:45:39 -0800

    refactored assertEqual, head function

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   if(array.length === 0) {
@@ -14,6 +8,4 @@ const head = function(array) {
   }
 }
 
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+module.exports= head;
