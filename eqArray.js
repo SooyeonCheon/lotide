7769f3e79d37b0e5b97076de4ac956cafb808c56
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-12 23:54:28 -0800

    eqArray recursive

--- eqArray.js
+++ eqArray.js
@@ -8,22 +8,49 @@ const assertEqual = function(actual, expected) {
 
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 
+/*
 const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log('false');
       return false;
     }
   }
-  console.log('true');
+  
   return true;
 };
+*/
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+// Recursive eqArrays
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else if (arr1.length === 0) {
+    return true;
+  } else if (Array.isArray(arr1[0]) && Array.isArray(arr2[0])) {
+    return eqArrays(arr1[0], arr2[0]) && eqArrays(arr1.slice(1), arr2.slice(1));
+  } else if (arr1[0] !== arr2[0]) {
+    return false;
+  } else {
+    return eqArrays(arr1.slice(1), arr2.slice(1));
+  }
+};
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
+// eqArrays([1, 2, 3], [1, 2, 3]); // => true
+// eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+// eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4, 5]], [[2, 3], [4, 5]]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
